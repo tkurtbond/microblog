a560373907d204f97aa111744ae4032aa15d3d95
T. Kurt Bond <tkurtbond@gmail.com>
2023-03-07 15:17:44 -0500

    Stop abusing [[]] for if-then, since an false [[]] leaves a failing exit code.
    And that messes up commands that are anded together, such as in ./do.

--- build.sh
+++ build.sh
@@ -6,9 +6,8 @@ cd cgi-bin # forth code assumes CWD is 'cgi-bin'
 
 for src in $(find ../gmi -type f -name '*.gmi'); do
 	target="$(dirname $src)/$(basename $src .gmi).html"
-        [[ $src -nt $target ]] && {
+        if [[ $src -nt $target ]]; then
                 echo Building $target
 	        cat $src | gforth render.fs -e 'stdin stdout lichen:render bye' > $target
-        }
-
+        fi
 done
