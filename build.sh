4459d52fdbcef23f665ba7b7054087376324079c
T. Kurt Bond <tkurtbond@gmail.com>
2023-03-07 14:50:21 -0500

    Only render the files that have changed.

--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@ cd cgi-bin # forth code assumes CWD is 'cgi-bin'
 
 for src in $(find ../gmi -type f -name '*.gmi'); do
 	target="$(dirname $src)/$(basename $src .gmi).html"
-	cat $src | gforth render.fs -e 'stdin stdout lichen:render bye' > $target
-	echo $target
+        [[ $src -nt $target ]] && {
+                echo Building $target
+	        cat $src | gforth render.fs -e 'stdin stdout lichen:render bye' > $target
+        } ||
+                echo Skipping $target
+
 done
